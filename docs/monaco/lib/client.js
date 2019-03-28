101688b6a97e42f3315fb2287704030a4fac9261
John Vilk <jvilk@stripe.com>
2019-03-28 12:16:42 -0700

    Restart Sorbet when it dies.
    Also, encapsulates Sorbet Emscripten bits into a class, and cleanly separates instantiation from message sending.

--- docs/monaco/lib/client.js
+++ docs/monaco/lib/client.js
@@ -1,12 +1,50 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var vscode_ws_jsonrpc_1 = require("vscode-ws-jsonrpc");
-var monaco_languageclient_1 = require("monaco-languageclient");
 var mock_socket_1 = require("mock-socket");
+var monaco_languageclient_1 = require("monaco-languageclient");
+var vscode_ws_jsonrpc_1 = require("vscode-ws-jsonrpc");
 var sorbet_1 = require("./sorbet");
 var ruby_1 = require("./ruby");
 ruby_1.register();
 var element = document.getElementById('editor');
+// NOTE: Important to define this variable up here due to function hoisting.
+// Persist the server in this variable so it does not get garbage collected.
+var mockServer = null;
 // Remove leading '#'
 var hash = window.location.hash.slice(1);
 var initialValue = hash ? decodeURIComponent(hash) : element.innerHTML;
@@ -19,6 +57,10 @@ var editor = monaco.editor.create(element, {
     scrollBeyondLastLine: false,
     formatOnType: true,
     autoIndent: true,
+<<<<<<< HEAD
+=======
+    lightbulb: { enabled: true },
+>>>>>>> Restart Sorbet when it dies.
     fontSize: 16,
 });
 window.addEventListener('hashchange', function () {
@@ -35,29 +77,38 @@ editor.onDidChangeModelContent(function (event) {
 });
 // install Monaco language client services
 monaco_languageclient_1.MonacoServices.install(editor);
-// create the web socket
-var webSocket = createFakeWebSocket();
-// listen when the web socket is opened
-vscode_ws_jsonrpc_1.listen({
-    webSocket: webSocket,
-    onConnection: function (connection) {
-        // create and start the language client
-        var languageClient = createLanguageClient(connection);
-        var disposable = languageClient.start();
-        connection.onClose(function () { return disposable.dispose(); });
-    }
-});
-sorbet_1.setRestartCallback(function () {
-    window.location.reload();
-});
+function startLanguageServer() {
+    console.log('Starting language server.');
+    // create the web socket
+    var webSocket = createFakeWebSocket();
+    // listen when the web socket is opened
+    vscode_ws_jsonrpc_1.listen({
+        webSocket: webSocket,
+        onConnection: function (connection) {
+            // create and start the language client
+            var languageClient = createLanguageClient(connection);
+            var disposable = languageClient.start();
+            connection.onClose(function () {
+                // vscode-ws-jsonrpc will try to re-connect to the server,
+                // but it tries to talk over the closed WebSocket and fails.
+                // Thus, we dispose of the language client and start a new language
+                // client instead.
+                disposable.dispose();
+                startLanguageServer();
+            });
+        }
+    });
+}
+startLanguageServer();
 function createLanguageClient(connection) {
     return new monaco_languageclient_1.MonacoLanguageClient({
-        name: "Sample Language Client",
+        name: 'Sample Language Client',
         clientOptions: {
             // use a language id as a document selector
             documentSelector: ['ruby'],
         },
-        // create a language client connection from the JSON RPC connection on demand
+        // create a language client connection from the JSON RPC connection on
+        // demand
         connectionProvider: {
             get: function (errorHandler, closeHandler) {
                 return Promise.resolve(monaco_languageclient_1.createConnection(connection, errorHandler, closeHandler));
@@ -66,19 +117,53 @@ function createLanguageClient(connection) {
     });
 }
 function createFakeWebSocket() {
+    var _this = this;
     var url = 'ws://sorbet.run:8080';
-    var mockServer = new mock_socket_1.Server(url);
-    mockServer.on('connection', function (socket) {
-        socket.on('message', function (message) {
-            sorbet_1.compile(function (response) {
-                console.log('Write: ' + response);
-                socket.send(response);
-            }).then(function (send) {
-                console.log('Read: ' + message);
-                send(message);
+    if (!mockServer) {
+        mockServer = new mock_socket_1.Server(url);
+        mockServer.on('connection', function (socket) { return __awaiter(_this, void 0, void 0, function () {
+            var sorbet_2, bufferedMessages_1, e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 2, , 3]);
+                        sorbet_2 = null;
+                        bufferedMessages_1 = [];
+                        // Socket doesn't buffer messages before a listener is attached.
+                        socket.on('message', function (message) {
+                            console.log('Read: ' + message);
+                            if (!sorbet_2) {
+                                // Sorbet is still initializing.
+                                bufferedMessages_1.push(message);
+                            }
+                            else {
+                                sorbet_2.sendMessage(message);
+                            }
+                        });
+                        return [4 /*yield*/, sorbet_1.default.create(function (response) {
+                                console.log('Write: ' + response);
+                                socket.send(response);
+                            }, function () {
+                                // Sorbet crashed.
+                                socket.close();
+                            })];
+                    case 1:
+                        sorbet_2 = _a.sent();
+                        // Send any messages that buffered during startup.
+                        while (bufferedMessages_1.length > 0) {
+                            sorbet_2.sendMessage(bufferedMessages_1.shift());
+                        }
+                        return [3 /*break*/, 3];
+                    case 2:
+                        e_1 = _a.sent();
+                        // Initialization failed. Close socket.
+                        socket.close();
+                        return [3 /*break*/, 3];
+                    case 3: return [2 /*return*/];
+                }
             });
-        });
-    });
+        }); });
+    }
     return new mock_socket_1.WebSocket(url);
 }
 //# sourceMappingURL=client.js.map
--- docs/monaco/src/sorbet.ts
+++ docs/monaco/src/sorbet.ts
@@ -4,76 +4,101 @@ declare var Sorbet: any;
 
 const sorbetWasmFile = fetch('../sorbet-wasm.wasm');
 
-const sorbetWasmModule = (typeof WebAssembly.compileStreaming == "function") ?
-  WebAssembly.compileStreaming(sorbetWasmFile) :
-  sorbetWasmFile
-  .then((response) => response.arrayBuffer())
-  .then((bytes) => WebAssembly.compile(bytes));
+const sorbetWasmModule = (typeof WebAssembly.compileStreaming == 'function') ?
+    WebAssembly.compileStreaming(sorbetWasmFile) :
+    sorbetWasmFile.then((response) => response.arrayBuffer())
+        .then((bytes) => WebAssembly.compile(bytes));
 
-let sorbet : any = null;
-let reservedFunction : any = null;
-let restartCallback : any = null;
-export function setRestartCallback(cb: () => void) {
-  restartCallback = cb;
-}
-const destroySorbet = () => {
-  sorbet = null;
-  reservedFunction = null;
-  if (restartCallback) {
-    restartCallback();
-  }
-}
-export const compile = (cb: (message: string) => void) => {
-  const send = (message: string) => {
-    sorbet.ccall('lsp', null, ['number', 'string'], [reservedFunction, message]);
-  }
+function NOP() {}
+export default class SorbetServer {
+  public static async create(
+      responseCallback: (response: string) => any,
+      errorCallback: (error: any) => any): Promise<SorbetServer> {
+    const sorbetServer = new SorbetServer();
+    sorbetServer.onResponse = responseCallback;
+    sorbetServer.onError = errorCallback;
 
-  return new Promise<(message: string) => void>((resolve, reject) => {
-    if (sorbet) {
-      // Already compiling or compiled
-      return resolve(send);
-    }
-
-    // For some unkonwn reason this varible has to be new everytime, and can't
-    // be out of the closure
-    const opts = {
-      print: (line: string) => {
-        console.log(line);
-        destroySorbet();
-      },
-      printErr: (line: string) => {
-        console.log(line);
-        destroySorbet();
-      },
-      onAbort: () => {
+    return new Promise<SorbetServer>((resolve, reject) => {
+      const opts = {
+        print: sorbetServer._print.bind(sorbetServer),
+        printErr: sorbetServer._printErr.bind(sorbetServer),
         // On abort, throw away our WebAssembly instance and create a
         // new one. This can happen due to out-of-memory, C++ exceptions,
-        // or other reasons; Throwing away and restarting should get us to a healthy state.
-        destroySorbet();
-      },
-      instantiateWasm: (info: any, realRecieveInstanceCallBack: any) => {
-        sorbetWasmModule
-          .then((module: any) =>
-            WebAssembly.instantiate(module, info)
-            .then((instance: any) => realRecieveInstanceCallBack(instance, module))
-            .catch((error: any) => {
-              console.log(error);
-            })
-          )
-          .catch((error: any) => {
-            console.log("Error loading sorbet.wasm. Maybe your adblock blocked it? Some of them are pretty aggressive on github.io domains. We promise we aren't mining crypto currencies on your computer.\n" + error);
-          });
-        return {}; // indicates lazy initialization
-      },
-      onRuntimeInitialized: () => {
-        reservedFunction = sorbet.addFunction((arg: any) => {cb(sorbet.Pointer_stringify(arg))}, "vi");
-        resolve(send);
-      }
-    };
+        // or other reasons; Throwing away and restarting should get us to a
+        // healthy state.
+        onAbort: sorbetServer._destroy.bind(sorbetServer),
+        instantiateWasm: (info: any, realReceiveInstanceCallBack: any) => {
+          instantiateWasmImpl(info, realReceiveInstanceCallBack).catch(reject);
+          return {};  // indicates lazy initialization
+        },
+        onRuntimeInitialized: () => {
+          resolve(sorbetServer);
+        }
+      };
 
-    sorbet = Sorbet(opts);
-  });
-};
+      // Since instantiateWasm requires an empty object as a return value,
+      // we can't make it async. So, instead, we put the async stuff here
+      // so we an use nice async/await syntax.
+      async function instantiateWasmImpl(
+          info: any, realReceiveInstanceCallBack: any): Promise<void> {
+        try {
+          const mod = await sorbetWasmModule;
+          const instance = await WebAssembly.instantiate(mod, info);
+          realReceiveInstanceCallBack(instance, mod);
+        } catch (error) {
+          console.log(
+              'Error loading sorbet.wasm. Maybe your adblock blocked it? Some of them are pretty aggressive on github.io domains. We promise we aren\'t mining crypto currencies on your computer.\n' +
+              error);
+          throw error;
+        }
+      }
+
+      sorbetServer._sorbet = Sorbet(opts);
+    });
+  }
+
+  private _destroyed: boolean = false;
+  private _sorbet: any;
+  public onResponse: (response: string) => any = NOP;
+  public onError: (error: any) => any = NOP;
+  private _onResponseEmscripten: any;
+
+  // Enforce construction via the static function.
+  private constructor() {}
+
+  private _print(line: string) {
+    console.log(line);
+    this._destroy();
+  }
+
+  private _printErr(line: string) {
+    console.log(line);
+    this._destroy();
+  }
+
+  private _destroy() {
+    if (!this._destroyed) {
+      this._destroyed = true;
+      this.onError('Sorbet shut down.');
+    }
+  }
+
+  public sendMessage(msg: string) {
+    if (this._destroyed) {
+      throw new Error('Sorbet server is not running.');
+    }
+
+    // Lazily initialize Emscripten-side of the response callback.
+    if (!this._onResponseEmscripten) {
+      this._onResponseEmscripten = this._sorbet.addFunction(
+          (arg: any) => {this.onResponse(this._sorbet.Pointer_stringify(arg))},
+          'vi');
+    }
+
+    this._sorbet.ccall(
+        'lsp', null, ['number', 'string'], [this._onResponseEmscripten, msg]);
+  }
+}
 
 document.getElementById('menu')!.addEventListener('click', (ev: any) => {
   ev.target.classList.toggle('is-showing');
