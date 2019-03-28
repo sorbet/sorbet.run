aaedf3d2d2c266a2b2fd62ff01b6584971d1e095
Paul Tarjan <github@paulisageek.com>
2019-03-28 10:43:40 -0700

    delete unused files

--- docs/monaco/lib/ext-json-server.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ext-json-server.d.ts.map
--- docs/monaco/lib/json-server-launcher.d.ts
+++
@@ -1,3 +0,0 @@
-import * as rpc from "vscode-ws-jsonrpc";
-export declare function launch(socket: rpc.IWebSocket): void;
-//# sourceMappingURL=json-server-launcher.d.ts.map
