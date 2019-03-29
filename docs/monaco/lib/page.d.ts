3a406c7e244baa7cd24b0b5a63ecfdb2c873f251
Paul Tarjan <github@paulisageek.com>
2019-03-29 01:10:45 -0700

    hide header on iframe

---
+++ docs/monaco/lib/page.d.ts
@@ -0,0 +1,1 @@
+//# sourceMappingURL=page.d.ts.map
--- docs/monaco/src/main.ts
+++ docs/monaco/src/main.ts
@@ -2,6 +2,7 @@
  * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+require('./page');
 require('monaco-editor-core');
 (self as any).MonacoEnvironment = {
     getWorkerUrl: () => './lib/editor.worker.bundle.js'
