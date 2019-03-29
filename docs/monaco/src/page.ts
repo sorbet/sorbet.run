307a2dbc8efba01e4fd7a7688477fbe18426327c
Paul Tarjan <github@paulisageek.com>
2019-03-29 10:41:48 -0700

    fix iframe

--- docs/monaco/src/page.ts
+++ docs/monaco/src/page.ts
@@ -1,7 +1,3 @@
 document.getElementById('menu')!.addEventListener('click', (ev: any) => {
   ev.target.classList.toggle('is-showing');
 });
-
-if (window.top != window.self) {
-  document.body.classList.add('iframe');
-}
