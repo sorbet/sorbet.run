56d2b2573665b1e3977740ba8442b3d15d1c0ece
Maruth Goyal <maruth@stripe.com>
2024-04-29 07:54:22 -0700

    oops

--- docs/monaco/lib/ruby.js
+++ docs/monaco/lib/ruby.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.register = void 0;
 var conf = {
     comments: { lineComment: '#', blockComment: ['=begin', '=end'] },
     brackets: [['(', ')'], ['{', '}'], ['[', ']']],
