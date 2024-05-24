298aa485ac77d272a60ddb4513b542dd8dc343b4
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-05-24 16:35:08 -0700

    npm run build

--- docs/monaco/lib/sorbet.d.ts
+++ docs/monaco/lib/sorbet.d.ts
@@ -2,7 +2,7 @@
  * Creates a new Sorbet instances. Calls errorCallback if Sorbet quits or
  * fails to start up.
  */
-export declare function createSorbet(onPrint: (line: string) => void, onError: (error: any) => void): Promise<{
+export declare function createSorbet(onPrint: (line: string) => void, onAbort: (error: any) => void): Promise<{
     sorbet: any;
 }>;
 //# sourceMappingURL=sorbet.d.ts.map
